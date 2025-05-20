faf3bab694f34c3803e798880db89dcef6c5dbb7
Jove Yu <yushijun110@126.com>
2025-05-20 15:33:04 +0800

    fix blink config

--- nvim-init.lua
+++ nvim-init.lua
@@ -28,7 +28,8 @@ require("lazy").setup({
 			"saghen/blink.cmp",
 			opts = {
 				keymap = {
-					preset = "super-tab",
+					["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+					["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 				},
 			},
 		},
