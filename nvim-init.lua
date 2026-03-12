49c8e0aa4a12a88776ab56f3aa162285878cde9a
Jove Yu <yushijun110@gmail.com>
2026-03-12 10:46:40 +0800

    update

--- nvim-init.lua
+++ nvim-init.lua
@@ -61,3 +61,13 @@ require("lazy").setup({
 
 vim.opt.relativenumber = false
 vim.g.autoformat = false
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = '»»',       -- Tab 键显示为 » 加一个空格
+  trail = '•',      -- 行尾多余的空格显示为点
+  nbsp = '␣',       -- 不换行空格
+  extends = '→',    -- 当设置了 nowrap 时，行向右延伸的指示
+  precedes = '←',   -- 当设置了 nowrap 时，行向左延伸的指示
+  -- eol = '↲',        -- 每一行的结尾显示换行符 (可选)
+}
+vim.opt.showbreak = "↳"
